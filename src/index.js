dae492cce163ec0940fef55fed3fd74f85a6b53d
Michael Cooper <macoop2363@gmail.com>
2020-02-20 10:29:32 -0700

    Add function to query for top words

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
 
+
+
 $(document).ready(() => {
   // have fun!
+  const POST_WORDS_PATH = "api/v1/words"
+
+  async function getTopWord() {
+    const url = new URL("https://wordwatch-api.herokuapp.com/api/v1/top_word")
+    return await fetch(url.href)
+      .then(data => { return data.json(); })
+      .catch(reason => { console.log(reason.message) });
+  }
+
+
 })
